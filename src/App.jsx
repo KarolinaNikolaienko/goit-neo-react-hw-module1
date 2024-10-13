f4cee0cac1fe6fa901e4aa6e08eb784428fc8dbd
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2024-10-13 23:17:18 +0200

    ver.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 import Profile from './components/Profile/Profile';
+import userData from './userData.json'
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json'
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json'
 
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
+// const userData = {
+//   username: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: "Ocho Rios, Jamaica",
+//   avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308
+//   }
+// };
 
 function App() {
 
@@ -24,6 +29,8 @@ function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </>
   )
 }
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+// import css from './FriendList.module.css'
+import FriendListItem from '../FriendListItem/FriendListItem'
+
+const FriendList = ({friends}) => {
+    return (
+        <>
+            <ul>
+                {friends.map((friend) => (
+				    <li key={friend.id}>
+                        <FriendListItem
+                            avatar={friend.avatar}
+                            name={friend.name}
+                            isOnline={friend.isOnline}
+                        />
+                    </li>
+			    ))}
+            </ul>
+        </>
+    )
+}
+
+export default FriendList
+                    
